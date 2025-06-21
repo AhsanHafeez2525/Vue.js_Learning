3d299f8b6bcb48f6aeba909d195b769c833341d9
Ahsan Hafeez Satti <ahsansatti402@gmail.com>
2025-06-21 21:56:33 +0500

    optionsApi static data

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "john doe",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>Vue job {{ name }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  background-color: aqua;
+  color: brown;
+}
+</style>
