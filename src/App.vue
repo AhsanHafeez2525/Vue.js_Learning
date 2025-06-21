ad1ef734c43a02b30a10e744a6e4100e75a88382
Ahsan Hafeez Satti <ahsansatti402@gmail.com>
2025-06-21 22:06:37 +0500

    Directives

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@ export default {
   data() {
     return {
       name: "john doe",
+      status: "active",
+      statusNow: "active",
+      tasks: ["Task one", "Task two", "Task three"],
+      link: "https://google.com",
     };
   },
 };
@@ -10,6 +14,18 @@ export default {
 
 <template>
   <h1>Vue job {{ name }}</h1>
+  <h4 v-if="status">the user is active {{ name }}</h4>
+  <h4 v-else-if="status === pending">the user is pending</h4>
+  <h4 v-else>the user is active</h4>
+
+  <h2>Tasks:</h2>
+  <ul>
+    <li v-for="task in tasks" key="task">{{ task }}</li>
+  </ul>
+
+  <a v-bind:href="link">Click for google</a>
+  <!-- short way -->
+  <a :href="link">Click for google</a>
 </template>
 
 <style scoped>
